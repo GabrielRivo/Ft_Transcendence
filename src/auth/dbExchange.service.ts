5a1f4038e53f458fc992db4968b837dff085dd54
WaRtr0 <mmorot@student.42lyon.fr>
2025-12-15 23:20:36 +0100

    chore: update dependencies and refactor DbExchangeService for improved type safety

--- src/auth/dbExchange.service.ts
+++ src/auth/dbExchange.service.ts
@@ -1,6 +1,6 @@
 import type { Database, RunResult, Statement } from 'better-sqlite3';
 import { InjectPlugin, Service } from 'my-fastify-decorators';
-import { providerKeys } from './providers.js';
+import { ProviderKeys } from './providers.js';
 
 @Service()
 export class DbExchangeService {
@@ -13,26 +13,40 @@ export class DbExchangeService {
 	private generateTokensPrepare: Statement<any>;
 	private getRefreshTokenPrepare: Statement<any>;
 	private getUserByProviderIdPrepare: Statement<any>;
-    private addUserByProviderIdPrepare: Statement<any>;
-    private findRefreshTokenPrepare: Statement<any>;
-    private getAllUsersPrepare: Statement<any>;
-    @InjectPlugin('db')
+	private addUserByProviderIdPrepare: Statement<any>;
+	private findRefreshTokenPrepare: Statement<any>;
+	private getAllUsersPrepare: Statement<any>;
+	@InjectPlugin('db')
 	private db!: Database;
 
 	onModuleInit() {
 		this.existingPrepare = this.db.prepare('SELECT id FROM users WHERE email = @email');
-		this.addUserPrepare = this.db.prepare('INSERT INTO users (email, password_hash) VALUES (@email, @password_hash)');
+		this.addUserPrepare = this.db.prepare(
+			'INSERT INTO users (email, password_hash) VALUES (@email, @password_hash)',
+		);
 		this.getUserByEmailPrepare = this.db.prepare('SELECT * FROM users WHERE email = @email');
-        this.getUserByIdPrepare = this.db.prepare('SELECT * FROM users WHERE id = @id');
-		this.storeRefreshTokenPrepare = this.db.prepare('INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES (@user_id, @token, @expires_at)');
-		this.revokeRefreshTokenPrepare = this.db.prepare('UPDATE refresh_tokens SET revoked = 1 WHERE token = @token');
+		this.getUserByIdPrepare = this.db.prepare('SELECT * FROM users WHERE id = @id');
+		this.storeRefreshTokenPrepare = this.db.prepare(
+			'INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES (@user_id, @token, @expires_at)',
+		);
+		this.revokeRefreshTokenPrepare = this.db.prepare(
+			'UPDATE refresh_tokens SET revoked = 1 WHERE token = @token',
+		);
 		this.generateTokensPrepare = this.db.prepare('SELECT * FROM users WHERE id = @id');
-		this.getRefreshTokenPrepare = this.db.prepare('SELECT * FROM refresh_tokens WHERE token = @token AND revoked = 0');
-		this.getUserByProviderIdPrepare = this.db.prepare('SELECT * FROM users WHERE provider = @provider AND provider_id = @provider_id');
-		this.addUserByProviderIdPrepare = this.db.prepare('INSERT INTO users (provider, provider_id, password_hash) VALUES (@provider, @provider_id, @password_hash)');
-        this.findRefreshTokenPrepare = this.db.prepare('SELECT * FROM refresh_tokens WHERE token = @token');
-        this.getAllUsersPrepare = this.db.prepare('SELECT id, email, provider, provider_id FROM users');
-    }
+		this.getRefreshTokenPrepare = this.db.prepare(
+			'SELECT * FROM refresh_tokens WHERE token = @token AND revoked = 0',
+		);
+		this.getUserByProviderIdPrepare = this.db.prepare(
+			'SELECT * FROM users WHERE provider = @provider AND provider_id = @provider_id',
+		);
+		this.addUserByProviderIdPrepare = this.db.prepare(
+			'INSERT INTO users (provider, provider_id, password_hash) VALUES (@provider, @provider_id, @password_hash)',
+		);
+		this.findRefreshTokenPrepare = this.db.prepare(
+			'SELECT * FROM refresh_tokens WHERE token = @token',
+		);
+		this.getAllUsersPrepare = this.db.prepare('SELECT id, email, provider, provider_id FROM users');
+	}
 
 	async existing(email: string) {
 		return this.existingPrepare.get({ email }) as { id: number } | undefined;
@@ -43,11 +57,15 @@ export class DbExchangeService {
 	}
 
 	async getUserByEmail(email: string) {
-		return this.getUserByEmailPrepare.get({ email }) as { id: number, email: string, password_hash: string } | undefined;
+		return this.getUserByEmailPrepare.get({ email }) as
+			| { id: number; email: string; password_hash: string }
+			| undefined;
 	}
 
-    async getUserById(id: number) {
-		return this.getUserByIdPrepare.get({ id }) as { id: number, email: string, password_hash: string } | undefined;
+	async getUserById(id: number) {
+		return this.getUserByIdPrepare.get({ id }) as
+			| { id: number; email: string; password_hash: string }
+			| undefined;
 	}
 
 	async storeRefreshToken(user_id: number, token: string, expires_at: string) {
@@ -55,38 +73,51 @@ export class DbExchangeService {
 	}
 
 	async revokeRefreshToken(token: string) {
-		return this.revokeRefreshTokenPrepare.run({ token  }) as RunResult;
+		return this.revokeRefreshTokenPrepare.run({ token }) as RunResult;
 	}
 
 	async generateTokens(id: number) {
-		return this.generateTokensPrepare.get({ id }) as { id: number, email: string, password_hash: string } | undefined;
+		return this.generateTokensPrepare.get({ id }) as
+			| { id: number; email: string; password_hash: string }
+			| undefined;
 	}
 
 	async getRefreshToken(token: string) {
-		return this.getRefreshTokenPrepare.get({ token }) as { id: number, user_id: number, token: string, expires_at: string, revoked: number } | undefined;
+		return this.getRefreshTokenPrepare.get({ token }) as
+			| { id: number; user_id: number; token: string; expires_at: string; revoked: number }
+			| undefined;
 	}
 
-    async getUserByProviderId(provider: providerKeys, provider_id: string) {
-		return this.getUserByProviderIdPrepare.get({ provider, provider_id }) as { id: number, email: string, password_hash: string } | undefined;
+	async getUserByProviderId(provider: ProviderKeys, provider_id: string) {
+		return this.getUserByProviderIdPrepare.get({ provider, provider_id }) as
+			| { id: number; email: string; password_hash: string }
+			| undefined;
 	}
 
-    async addUserByProviderId(provider: providerKeys, provider_id: string) {
-		console.log("addUserByProviderId",provider,provider_id);
+	async addUserByProviderId(provider: ProviderKeys, provider_id: string) {
+		console.log('addUserByProviderId', provider, provider_id);
 		try {
-			return this.addUserByProviderIdPrepare.run({ provider, provider_id, password_hash: "" }) as RunResult;
+			return this.addUserByProviderIdPrepare.run({
+				provider,
+				provider_id,
+				password_hash: '',
+			}) as RunResult;
 		} catch (error) {
-			console.error("Error adding user by provider id",error);
+			console.error('Error adding user by provider id', error);
 			// throw new BadGatewayException(`${provider} - TA OUBLIER UN TRUC POUR LE PROVIDER!`);
 		}
 		return this.addUserByProviderIdPrepare.run({ provider, provider_id }) as RunResult;
 	}
 
-    async findRefreshToken(token: string) {
-		return this.findRefreshTokenPrepare.get({ token }) as { id: number, user_id: number, token: string, expires_at: string, revoked: number } | undefined;
+	async findRefreshToken(token: string) {
+		return this.findRefreshTokenPrepare.get({ token }) as
+			| { id: number; user_id: number; token: string; expires_at: string; revoked: number }
+			| undefined;
 	}
 
-    async getAllUsers() {
-		return this.getAllUsersPrepare.all({}) as { id: number, email: string, provider: providerKeys, provider_id: string }[] | undefined;
+	async getAllUsers() {
+		return this.getAllUsersPrepare.all({}) as
+			| { id: number; email: string; provider: ProviderKeys; provider_id: string }[]
+			| undefined;
 	}
 }
-
