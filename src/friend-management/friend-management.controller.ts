33d5595909578151724d68f074ba8d6e97affe6e
Romane Golfetto <rgolfett@z4r3p4.42lyon.fr>
2025-12-21 17:46:01 +0100

    added security check

--- src/friend-management/friend-management.controller.ts
+++ src/friend-management/friend-management.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Inject, Post } from 'my-fastify-decorators';
+import { Body, BodySchema, Controller, Delete, Get, Inject, Param, Post } from 'my-fastify-decorators';
 import { FriendManagementService } from './friend-management.service.js';
+import { AddFriendSchema, AddFriendDto } from './dto/addFriend.dto.js';
+
 
 @Controller('/friend-management')
 export class FriendManagementController {
@@ -7,16 +9,23 @@ export class FriendManagementController {
 	private friend_managementService !: FriendManagementService;
 
 	@Post('/friend')
-	add_friend(@Body() data : {userId : number, otherId : number}) {
-		try {
-			this.friend_managementService.add_friend(data.userId, data.otherId);
-		} catch (e) {
-			console.log(e)
-		}
-	}
+	@BodySchema(AddFriendSchema)
+	add_friend(@Body() data : AddFriendDto) {
 
+		return this.friend_managementService.add_friend(data.userId, data.otherId);
+ 
+			
+	}
+ 
+	// @Get('/friend/:id')
+	// get_friend(@Param("id") id : number){
+	// 	return { 
+	// 		message : `L'id v2 est ${id}`
+	// 	}
+	// } 
+ 
 	@Delete('/friend')
 	delete_friend(@Body() data : {userId : number, otherId : number}) {
 		// this.friend_managementService.delete_friend();
 	}
-}    
+}     
