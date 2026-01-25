409760f7cd821868de5786177b0a6ed28f9d06c0
mmorot <mmorot@student.42lyon.fr>
2026-01-25 21:17:49 +0100

    feat(rabbitmq): integrate RabbitMQ plugin and enhance user consumer with event handling for user lifecycle events

--- src/plugin-manager.ts
+++ src/plugin-manager.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from 'fastify';
 
 import multipartPlugin from './plugins/multipart-plugin.js';
+import rabbitmqPlugin from './plugins/rabbitmq-plugin.js';
 import socketPlugin from './plugins/socket-plugin.js';
 import sqlitePlugin from './plugins/sqlite-plugin.js';
 
@@ -8,5 +9,6 @@ export default function pluginManager(app: FastifyInstance, options: { dbPath?: 
 	app.register(sqlitePlugin, { dbPath: options.dbPath });
 	app.register(socketPlugin);
 	app.register(multipartPlugin);
+	app.register(rabbitmqPlugin);
 	// app.register(jwtPlugin);
 }
