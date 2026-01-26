f71e47a523a64a8ca6c7281d8dbe4741fbb8a2e9
WaRtr0 <mmorot@student.42lyon.fr>
2026-01-26 16:17:45 +0100

    feat: Implement OTP email sending via Resend API

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { createWorker } from 'my-fastify-decorators-microservices';
-import { AppModule } from './app.module.js';
 import { Resend } from 'resend';
+import { AppModule } from './app.module.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 createWorker({
-    module: AppModule,
-    rabbitMQ: {
-        urls: [process.env.RABBITMQ_URL!],
-        queue: 'mail_queue',
-    },
-    globals: {
+	module: AppModule,
+	rabbitMQ: {
+		urls: [process.env.RABBITMQ_URL!],
+		queue: 'mail_queue',
+	},
+	globals: {
 		resend,
-        // Si tu utilises @InjectPlugin('db'), tu peux passer l'objet ici
-    }
+		// Si tu utilises @InjectPlugin('db'), tu peux passer l'objet ici
+	},
 }).catch((err: any) => {
-    console.error(err);
-    process.exit(1);
+	console.error(err);
+	process.exit(1);
 });
