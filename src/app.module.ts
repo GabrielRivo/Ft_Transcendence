40e37f4a65f9306e09f45108ec1c6cb677192a75
rsequeir <rsequeir@student.42lyon.fr>
2026-01-28 09:13:25 +0100

    Dockerfile.prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from 'my-fastify-decorators';
-import { HealthController } from './health.controller.js';
 import { TournamentModule } from './tournament/tournament.module.js';
 
 @Module({
 	// providers: [TournamentModule],
 	imports: [TournamentModule],
-	controllers: [HealthController],
 })
 export class AppModule { }
--- src/health.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from 'my-fastify-decorators';
-
-@Controller('/health')
-export class HealthController {
-	@Get('/')
-	public async getHealth() {
-		return { status: 'ok' };
-	}
-}
